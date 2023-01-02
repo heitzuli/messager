2420b4156a779ccab3a2bbf65f7a8cfd95bd2af5
Rudolf Poels <rudolfpoels@rudolfs-mbp-2.lan>
2023-01-02 18:15:54 +0200

    🌅 initial commit

---
+++ src/test/java/fi/rehuapro/messager/MessagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.rehuapro.messager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
