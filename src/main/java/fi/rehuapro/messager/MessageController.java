ed4791a70bf65209a08af3dd0e1f8e62412d8715
heidi <heidi.poels@gmail.com>
2023-01-02 19:41:56 +0200

    Create new message

--- src/main/java/fi/rehuapro/messager/MessageController.java
+++ src/main/java/fi/rehuapro/messager/MessageController.java
@@ -2,6 +2,7 @@ package fi.rehuapro.messager;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -10,11 +11,25 @@ import java.util.Map;
 @RestController
 @Validated
 public class MessageController {
+    // Key = id, value = message
     private static final Map<Long, Message> messageMap = new HashMap<>();
 
     @GetMapping("/message")
     public Map<Long, Message> messages() {
         return messageMap;
+    }
+
+    /**
+     * Creates a new message and saves it to database
+     * @param content is the body of the message
+     * @return the generated id for the message
+     */
+    @PostMapping("/message")
+    public Long createMessage(String content) {
+        var id = 0L;
+        var message = new Message(id, content);
+        messageMap.put(id, message);
+        return id;
     }
 
 }
