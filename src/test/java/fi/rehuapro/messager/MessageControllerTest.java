fa5cbda4d79cf783821884bd5e65138fc9a0889b
heidi <heidi.poels@gmail.com>
2023-01-03 20:04:20 +0200

    Test message creation

---
+++ src/test/java/fi/rehuapro/messager/MessageControllerTest.java
@@ -0,0 +1,37 @@
+package fi.rehuapro.messager;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MessageControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void createMessage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .post("/message")
+                .content(asJsonString(new Message(null, "My penguin")))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    public String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
