a61ee4df7c04c9bf63444f2a41ee410365bdc3df
heidi <heidi.poels@gmail.com>
2023-01-03 19:16:06 +0200

    Add get message

--- src/main/java/fi/rehuapro/messager/MessageController.java
+++ src/main/java/fi/rehuapro/messager/MessageController.java
@@ -1,10 +1,7 @@
 package fi.rehuapro.messager;
 
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +29,16 @@ public class MessageController {
         var message = new Message(id, requestMessage.content());
         messageMap.put(id, message);
         return id;
+    }
+
+    /**
+     * Gets message based on id
+     * @param id is the id of a message
+     * @return the message
+     */
+    @GetMapping("/message/{id}")
+    public Message getMessage(@PathVariable String id) {
+        return messageMap.get(id);
     }
 
 }
